ef346bae8e53312794f16c515a149c8d8dfa763b
Carlin <email.me@carlinyuen.com>
2015-03-12 23:56:57 -0400

    ditching rich html editor, too much work

--- js/demo.js
+++ js/demo.js
@@ -1,37 +1,8 @@
 
 (function($)
 {
-    ///////////////////
-    // Functions
-   
-    // Take selection and wrap in tags
-    function wrapSelectionInTag(tag, selection)
-    {
-    }
-
     ////////////////////////
     // Setup editor buttons
-    
-    $('#boldButton').click(function(event) 
-    {
-        console.log("boldButton clicked");
-    });
-
-    $('#italicizeButton').click(function(event) 
-    {
-        console.log("italicizeButton clicked");
-    });
-
-    $('#underlineButton').click(function(event) 
-    {
-        console.log("underlineButton clicked");
-    });
-
-    $('#linkButton').click(function(event) 
-    {
-        console.log("linkButton clicked");
-    });
-
     $('#convertHTMLButton').click(function(event) 
     {
         console.log("convertHTMLButton clicked");
