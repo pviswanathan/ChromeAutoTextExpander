7fb20c2083184c9ddbf2b5515f12fe591366121b
Carlin <me@carlinyuen.com>
2019-06-27 23:59:51 -0400

    testing tests framework

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,26 @@
+// Browser-based tests
+
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+  console.log('Browser-based Tests');
 
+  describe('initialization', function() {
+    describe('jquery init', function() {
+      it('should be available as a function', function() {
+        expect(typeof window.jQuery).to.equal('function');
+      });
+    });
+    describe('moment init', function() {
+      it('should be available as a function', function() {
+        expect(typeof window.moment).to.equal('function');
+      });
+    });
+    describe('constants init', function() {
+      it('should have PROD chrome extension ID', function() {
+        expect(APP_ID_PRODUCTION).to.equal('iibninhmiggehlcdolcilmhacighjamp');
       });
     });
   });
+
 })();
